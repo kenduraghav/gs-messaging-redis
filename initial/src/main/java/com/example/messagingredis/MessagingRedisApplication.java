23d05182b7060465c31eac9dd216f69c51249c23
kenduraghav <kenduraghav@gmail.com>
2024-04-26 16:48:18 +0800

    Demo Application with Redis

--- initial/src/main/java/com/example/messagingredis/MessagingRedisApplication.java
+++ initial/src/main/java/com/example/messagingredis/MessagingRedisApplication.java
@@ -1,13 +1,64 @@
 package com.example.messagingredis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class MessagingRedisApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(MessagingRedisApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessagingRedisApplication.class, args);
+	public static void main(String[] args) throws InterruptedException {
+//		SpringApplication.run(MessagingRedisApplication.class, args);
+		
+		ApplicationContext ctx = SpringApplication.run(MessagingRedisApplication.class, args);
+
+		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+		MessageReceiver receiver = ctx.getBean(MessageReceiver.class);
+
+		while (receiver.getCount() == 0) {
+
+			LOGGER.info("Sending message...");
+			template.convertAndSend("chat", "Hello from Redis!");
+			Thread.sleep(500L);
+		}
+
+		System.exit(0);
 	}
+	
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter listenerAdapter) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(redisConnectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+		return container;
+	}
+	
+	
+	@Bean
+	MessageListenerAdapter listenerAdapter(MessageReceiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+	
+	@Bean
+	StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+	
+	@Bean
+	public MessageReceiver receiver() {
+		return new MessageReceiver();
+	}
+	
+	
 
 }
